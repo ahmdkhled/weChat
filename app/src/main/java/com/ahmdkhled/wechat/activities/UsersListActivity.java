5cf6f577c16151a41c140ddd3be7bd5bf232e832
ahmdkhled <ahmdkhled@gmail.com>
2018-06-29 21:59:04 +0200

    feat: add search feature for users list

--- app/src/main/java/com/ahmdkhled/wechat/activities/UsersListActivity.java
+++ app/src/main/java/com/ahmdkhled/wechat/activities/UsersListActivity.java
@@ -1,12 +1,25 @@
 package com.ahmdkhled.wechat.activities;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.text.style.ForegroundColorSpan;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.ahmdkhled.wechat.R;
@@ -27,15 +40,41 @@ public class UsersListActivity extends AppCompatActivity implements UsersListAda
     ArrayList<User> usersList;
     UsersListAdapter usersAdapter;
     RecyclerView usersRecycler;
+    EditText searcBox;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_users_list);
         usersRecycler=findViewById(R.id.userRecycler);
+        searcBox=findViewById(R.id.userSearchBox_ET);
         usersList=new ArrayList<>();
         root= FirebaseDatabase.getInstance().getReference().getRoot();
 
+        getSupportActionBar().setElevation(0);
+        SpannableString s = new SpannableString("Users");
+        s.setSpan(new ForegroundColorSpan(Color.parseColor("#000000")), 0, 5, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        getSupportActionBar().setTitle(s);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_arrow_back_black_24dp);
+
         getUsers();
+
+        searcBox.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                ArrayList<User> users= filterUsers(charSequence.toString());
+                showUsers(users);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+            }
+        });
+
     }
 
     void getUsers(){
@@ -79,6 +118,18 @@ public class UsersListActivity extends AppCompatActivity implements UsersListAda
     }
     String getCurrentUserUid(){
         return FirebaseAuth.getInstance().getCurrentUser().getUid();
+    }
+
+    ArrayList<User> filterUsers(String search){
+        if (TextUtils.isEmpty(search))
+            return usersList;
+        ArrayList<User> users=new ArrayList<>();
+        for (User user:usersList){
+            if (user.getName().contains(search)){
+                users.add(user);
+            }
+        }
+        return users;
     }
 
     @Override
