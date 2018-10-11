5126c68cc34f2955120096b5765043e406dbf0a2
ahmdkhled <ahmdkhled@gmail.com>
2018-10-11 17:33:07 +0200

    fix design of sigup activity

--- app/src/main/java/com/ahmdkhled/wechat/activities/LoginActivity.java
+++ app/src/main/java/com/ahmdkhled/wechat/activities/LoginActivity.java
@@ -1,6 +1,5 @@
 package com.ahmdkhled.wechat.activities;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputLayout;
--- app/src/main/java/com/ahmdkhled/wechat/activities/SignupActivity.java
+++ app/src/main/java/com/ahmdkhled/wechat/activities/SignupActivity.java
@@ -13,8 +13,6 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.ahmdkhled.wechat.R;
-import com.ahmdkhled.wechat.model.Post;
-import com.ahmdkhled.wechat.model.User;
 import com.ahmdkhled.wechat.utils.Encryption;
 import com.ahmdkhled.wechat.utils.Utils;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -23,13 +21,9 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.iid.FirebaseInstanceId;
-
 import java.util.HashMap;
 import java.util.Map;
 
